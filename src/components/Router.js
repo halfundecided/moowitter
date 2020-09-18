b27ccd028557490cb99c2a547b0b9b3044110d3b
Mijeong Ban <mban1@stevens.edu>
2020-09-18 11:36:48 -0400

    Basic router setup

---
+++ src/components/Router.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+export default () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+    <Router>
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
