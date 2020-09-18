1e5edb89574794292cb5cf5e05d5b881bf7dcc18
Mijeong Ban <mban1@stevens.edu>
2020-09-18 11:40:04 -0400

    Basic router setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import MoowitterRouter from "./Router";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <MoowitterRouter />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "../routes/Home";
+import Auth from "../routes/Auth";
 
-export default () => {
+const MoowitterRouter = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <Router>
@@ -21,3 +23,5 @@ export default () => {
     </Router>
   );
 };
+
+export default MoowitterRouter;
